bcf5c8ea8994ca214d2e03f00e1c4e2de573948a
SilWolf <silwolf1121@gmail.com>
2022-07-26 02:49:05 +0800

    feat: on/off app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
-import * as React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import PostDetailPage from './pages/postDetail'
+import { PostContextProvider } from './hooks/useBahaPost'
 
 import './app.css'
-import { PostContextProvider } from './hooks/useBahaPost'
 
 const children = <PostDetailPage />
 
 const App = () => {
-  return <PostContextProvider>{children}</PostContextProvider>
+  const [isAppOpened, setIsAppOpened] = React.useState<boolean>(false)
+
+  useEffect(() => {
+    if (isAppOpened) {
+      document.body.style.overflow = 'hidden'
+
+      return () => {
+        document.body.style.removeProperty('overflow')
+      }
+    }
+  }, [isAppOpened])
+
+  const handleClickClose = useCallback(() => setIsAppOpened(false), [])
+
+  useEffect(() => {
+    const container = document.querySelector(
+      '.main-container_wall-post_header_main'
+    )
+    if (container) {
+      const openButton = document.createElement('div')
+      openButton.classList.add('baha-hoverbtn-features')
+      openButton.innerHTML = 'O'
+      openButton.addEventListener('click', () => setIsAppOpened(true))
+
+      container.append(openButton)
+    }
+  }, [])
+
+  return (
+    <PostContextProvider>
+      <div
+        className={`fixed left-0 right-0 bg-white p-4 pt-12 transition-[top,bottom,opacity] ${
+          isAppOpened
+            ? 'z-30 top-0 bottom-0 opacity-100'
+            : '-z-30 top-[400px] -bottom-[400px] opacity-0'
+        }`}
+      >
+        <div className="w-full h-full flex flex-col min-h-0">
+          <div className="py-2 text-right">
+            <button onClick={handleClickClose}>關閉</button>
+          </div>
+          <div className="flex-1 overflow-auto min-h-0">{children}</div>
+        </div>
+      </div>
+    </PostContextProvider>
+  )
 }
 
 export default App
--- src/layouts/master.layout.tsx
+++ src/layouts/master.layout.tsx
@@ -1,23 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 type Props = {
   children?: React.ReactNode
 }
 
 const MasterLayout = ({ children }: Props) => {
-  useEffect(() => {
-    document.body.style.overflow = 'hidden'
-
-    return () => {
-      document.body.style.removeProperty('overflow')
-    }
-  }, [])
-
-  return (
-    <div className="fixed z-30 inset-0 bg-white p-4 pt-12">
-      <div className="container">{children}</div>
-    </div>
-  )
+  return <div className="container">{children}</div>
 }
 
 export default MasterLayout
